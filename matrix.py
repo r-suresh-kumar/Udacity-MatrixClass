d81df593143fac0832e3c718e4d26fe430a223a0
r-suresh-kumar <54197649+r-suresh-kumar@users.noreply.github.com>
2019-08-19 22:55:13 +0530

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,21 @@
+import math
+from math import sqrt
+import numbers
+
+def zeroes(height, width):    
+        """
+        Creates a matrix of zeroes.
+        """
+        g = [[0.0 for _ in range(width)] for __ in range(height)]
+        return Matrix(g)
+
+def identity(n):    
+        """
+        Creates a n x n identity matrix.
+        """
+        I = zeroes(n, n)
+        for i in range(n):
+            I.g[i][i] = 1.0
+        return I
+    
+    
